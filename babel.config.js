3b5549c7db6e7c86b0130d1b3652ca06ea7a0647
thebadone231 <chenhaoli123@gmail.com>
2022-05-29 22:31:20 +0800

    added config for Heroku deployment

--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,5 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    // resolver: {
-    //   sourceExts: ['jsx', 'js', 'ts', 'tsx'], //add here
-    // },
   };
 };
